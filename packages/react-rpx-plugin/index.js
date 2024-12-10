30b2a45c32df859efad74f38638012852d9bd14f
周晓磊 <690039872@qq.com>
2024-12-10 18:15:34 +0800

    feat:plugin

--- packages/react-rpx-plugin/index.js
+++ packages/react-rpx-plugin/index.js
@@ -1,3 +1,42 @@
-const compilationLoader = require('./src/loader/compilationLoader');
+const { transformInclude } = require('./src/utils');
 
-module.exports = compilationLoader;
+class ReactJSXTransferPlugin {
+  mode = 'production';
+  runtime = '';
+
+  constructor(options = {}) {
+    const { mode, runtime } = options;
+
+    if (['production', 'development'].includes(mode)) {
+      this.mode = mode;
+    }
+
+    if (['automatic', 'classic'].includes(runtime)) {
+      this.runtime = runtime;
+    }
+  }
+
+  apply(compiler) {
+    // jsx-runtime
+    if (this.runtime !== 'automatic') {
+      console.log('当前react版本不支持jsx-runtime');
+      return;
+    }
+
+    const { options } = compiler;
+
+    // add jsx-runtime loader
+    const runtimeOption = {
+      test: transformInclude,
+      use: {
+        loader: require.resolve('./src/loader/compilationLoader'),
+        options: {
+          mode: this.mode,
+        },
+      },
+    };
+    options.module.rules.push(runtimeOption);
+  }
+}
+
+module.exports = ReactJSXTransferPlugin;
